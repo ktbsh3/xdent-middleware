4381453cb305beac41b65de95ce74fb09c4b0147
Jan Samoel <jan.samoel@gmail.com>
2019-11-05 18:38:45 +0100

    aaaaaaaaaa

--- hyohin/routes/index.js
+++ hyohin/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var data = fs.readFileSync("store/data.json");
+var jsoned = JSON.parse(data.toString());
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', message: "HENLOL", data: jsoned });
 });
 
 module.exports = router;
